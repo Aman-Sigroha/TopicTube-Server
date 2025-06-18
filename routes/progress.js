dda3eccb77c46eff5cd40d6c51605db4a091075a
Aman-Sigroha <amansigroha4@gmail.com>
2025-06-18 21:56:47 +0530

    fixing error in deploy

--- routes/progress.js
+++ routes/progress.js
@@ -1,1 +1,9 @@
- 
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const progressController = require('../controllers/progressController');
+
+router.get('/', auth, progressController.getProgress);
+router.post('/', auth, progressController.saveProgress);
+
+module.exports = router; 
